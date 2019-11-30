106572246b626ab4f12c7bfab8c68922b10ec086
David Green <david@GreenGiantGraphics.com>
2019-11-29 21:06:01 -0600

    added website files

---
+++ wwwroot/js/index.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () { 
+var x = 0;
+var s = "";
+
+console.log("Hello Bit Main Street");
+
+
+var theform = $("#theForm");
+
+theform.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
+
+    var $loginToggle = $("#loginToggle");
+    var $popupform = $(".popupform");
+
+    $loginToggle.on("click", function () {
+        $popupform.fadeToggle(1000);
+    });
+
+});
